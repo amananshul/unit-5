ae3eda085b47243fd8409792db759dfb8a05fa17
amananshul <80920301+amananshul@users.noreply.github.com>
2022-05-23 15:46:53 +0530

    ok

--- Day4/day4/src/App.tsx
+++ Day4/day4/src/App.tsx
@@ -1,31 +1,56 @@
-import React, { useMemo, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useList } from "./hooks/useList";
+import { useCounter } from "./hooks/useCounter";
+import { v4 as uuid } from "uuid";
 
 function App() {
-  const [text, setText] =useState("")
+  // Entire thing bellow should work. you don't have to edit this file, only implement  hooks
+  const [list, { push, pop, clear, reset, map }]: any = useList([1, 2, 3]);
 
+  const { value, inc, dec, set } = useCounter(10);
 
-  const fibo=(n:number):number =>{
-    if(n<=1){
-      return n 
-
-    }
-    return fibo(n-1)+fibo(n-2)
-  }
-  
-  const memoisedFibo = useMemo(()=>{
-    return fibo(Number(text))
-  },[text])
-  console.time("t1")
-  console.timeEnd("T2")
-  const calcFibo = ()=>{
-    console.log("nth", memoisedFibo)
-  }
   return (
-    <div>
-   <input type="text" value={text} onChange={(e)=>{setText(e.target.value)}} />
-   <button onClick={calcFibo}>Calc</button>
+    <div>    <button
+      
+        onClick={() => push(list.length + 1)}
+      >
+        Append
+      </button>
+      <button onClick={() => pop()}>
+        pop
+      </button>
+      <button onClick={() => clear()}>
+        clear
+      </button>
+      <button onClick={() => reset()}>
+        reset
+      </button>
+      <button
+      
+        onClick={() => map((el: any) => el * 2)}
+      >
+        Double every element
+      </button>
+      {list.map((l: any) => (
+        <div key={uuid()}>{l}</div>
+      ))}
+
+      <hr />
+      <button onClick={() => inc()}>
+        Add 1
+      </button>
+      <button onClick={() => inc(3)}>
+        Add 3
+      </button>
+      <button onClick={() => dec()}>
+        Sub 1
+      </button>
+      <button onClick={() => dec(4)}>
+        Sub 4
+      </button>
+      <button onClick={() => set(100)}>
+        Set counter to 100
+      </button>
+      <h3>Value: {value}</h3>
     </div>
   );
 }
