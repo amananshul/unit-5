1d2738d4f3c721d5eaa179fd4b784f7ec33a3acf
aman <aman004dubey@gmail.com>
2022-06-02 10:23:43 +0530

    ok

--- Day1Node/index.js
+++ Day1Node/index.js
@@ -12,22 +12,32 @@
 // console.log(isEven(10))
 // const {a}= require('./an')
 // console.log(a)
-const http=require("http");
 const fs=require('fs')
+const http=require("http");
+
 const server = http.createServer((req, res) => {
-    if(req.url==="/"){
-         fs.readFile("./index.html",{encoding: 'utf8'},(err, data) => {
-             if(err) throw err
-                 res.write(data)
-                 res.end("hi")
-       })
+    console.log(req.url)
+    if(req.url=='/'){
+        res.setHeader("content-type", "text/plain")
+     return   res.end("hello") 
     }
-    if(req.url==="/products"){
-        res.end("Hello")
+    if(req.url=='/prod'){
+        res.setHeader("content-type", "application/json")
+      return  res.end("[1,2,3,4,5]")
+
+    }
+    if(req.url=='/file'){
+        const readStream=fs.createReadStream('./index.html')
+        readStream.pipe(res)
+        // const data= fs.readFile('./index.html',{encoding:'utf-8'},(err,data)=>{
+        //     if(err) {console.log("error")}
+        //     res.setHeader('Content-Type', 'text/html')
+        //     return res.end(data)
+        // })
     }
         
-    })
-//   
+ })
+  
 server.listen(8080,()=>{
     console.log("server started on http://localhost:8080/")
 })
