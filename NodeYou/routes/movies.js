42761e81fe3719c3678cc4e1b649f8b46bb31abb
aman <aman004dubey@gmail.com>
2022-06-12 00:17:28 +0530

    ok

--- NodeYou/routes/movies.js
+++ NodeYou/routes/movies.js
@@ -22,4 +22,30 @@ router.post('/',(req,res)=>{
         res.status(500).send('movie not found in db')
     })
 })
+router.get("/",(req,res)=>{
+   Movie.find()
+   .then((movies)=>res.send(movies))
+   .catch((error)=>{
+    res.status(500).send("something went wrong")
+    console.log("error")
+})
+})
+
+router.get("/:movieId",async(req,res)=>{
+   const movie= await Movie.findById(req.params.movieId)
+   if(!movie) res.send("movie not found")
+    res.send(movie)
+
+    // We can do it by .then catch too
+    // .then((movie)=>{
+        // if(movie) res.send(movie)
+        // res.status(403).send("movie not found")
+        // }).catch((error)=>{
+        //     res.status(500).send(error.message)
+        // });
+})
+router.put("/:movieId",async(req,res)=>{
+    let update = await Movie.findById.
+})
+
 module.exports=router;
